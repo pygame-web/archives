5b3cb4931d069af487c05fb934b1e1c75717204b
pmp-p <mail.peny@free.fr>
2022-10-06 18:21:24 +0200

    .

--- 0.4/pythonrc.py
+++ 0.4/pythonrc.py
@@ -11,7 +11,6 @@ import time
 PYCONFIG_PKG_INDEXES_DEV =['http://localhost:<port>/archives/repo/']
 PYCONFIG_PKG_INDEXES = ['https://pygame-web.github.io/archives/repo/']
 
-
 # the sim does not preload assets and cannot access currentline
 # unless using https://github.com/pmp-p/aioprompt/blob/master/aioprompt/__init__.py
 
@@ -1012,7 +1011,7 @@ if not aio.cross.simulator:
         def get_pkg(cls, want, ex=None, resume=None):
             return cls.async_get_pkg(want, ex, resume)
 
-        async def pv(track, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+        async def pv(track, prefix = '', suffix = '', decimals = 1, length = 100, fill = 'X', printEnd = "\r"):
 
             # Progress Bar Printing Function
             def print_pg_bar(total, iteration):

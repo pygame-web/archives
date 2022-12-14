d627501231b695187f101c269b66a1eadee6e97f
pmp-p <mail.peny@free.fr>
2022-12-14 20:41:20 +0100

    add raypyc

--- server.py
+++ server.py
@@ -19,9 +19,10 @@ import io
 import os
 
 import urllib
-
+import urllib.request
 
 from pathlib import Path
+import hashlib
 
 CWD = os.getcwd()
 
@@ -34,6 +35,9 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+
+CACHE = Path("cache")
+CACHE.mkdir(parents=True, exist_ok=True)
 
 class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -57,7 +61,25 @@ class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
 
     def send_head(self):
         path = self.translate_path(self.path)
-        print(path)
+
+        ctype = self.guess_type(path)
+        content = b''
+
+        def flush():
+            nonlocal ctype
+            nonlocal content
+            self.send_header("content-type", ctype)
+            self.send_header("content-length", str(len(content)))
+            self.end_headers()
+            return io.BytesIO(content)
+
+
+
+        if self.path.endswith('.map'):
+            self.send_response(404)
+            self.send_header("content-length", 0)
+            self.end_headers()
+            return io.BytesIO(b'')
 
         if os.path.isdir(path):
             parts = urllib.parse.urlsplit(self.path)
@@ -77,22 +99,39 @@ class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
             else:
                 return self.list_directory(path)
 
-        ctype = self.guess_type(path)
+        if not Path(path).is_file():
+            self.send_response(404)
+            return flush()
 
         self.send_response(HTTPStatus.OK)
 
-        with open(path, "rb") as f:
-            content = f.read()
-            if path.endswith(".html"):
+        if self.path.startswith('/cors/'):
+            ctype = 'text/plain; charset=utf-8'
+
+            target = self.path[6:]
+            print("CORS:", target)
+            cache = CACHE / hashlib.md5(target.encode()).hexdigest()
+            if cache.is_file():
+                path = cache
+            else:
+                if target.startswith('https://paste.pythondiscord.com/'):
+                    if not target.count('/raw/'):
+                        target = target.replace('https://paste.pythondiscord.com/','https://paste.pythondiscord.com/raw/',1)
+                else:
+                    content = f'print("Unsupported paste service")'.encode()
+                    return flush()
+
+                path, headers = urllib.request.urlretrieve(target, cache)
+
+        with open(path, "rb") as file:
+            content = file.read()
+            if Path(path).suffix in [".html"]:
                 content = content.replace(
                     b"https://pygame-web.github.io/", b"http://localhost:8000/"
                 )
 
-        self.send_header("content-type", ctype)
-        self.send_header("content-length", str(len(content)))
-        self.end_headers()
+        return flush()
 
-        return io.BytesIO(content)
 
 
 server.test(HandlerClass=MyHTTPRequestHandler, protocol="HTTP/1.1", port=args.port)

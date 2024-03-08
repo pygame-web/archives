34959bf3e4f98df5c3e894999eaf1e7daf2ea4fc
pmp-p <mail.peny@free.fr>
2024-03-08 18:16:32 +0100

    fix abi3 paths

--- repo/buildmap.py
+++ repo/buildmap.py
@@ -135,14 +135,17 @@ def process_wheel(whl, whlname):
 
 for whl in Path(".").glob("pkg/*.whl"):
     whlname = whl.as_posix()
-
+    abi3 = False
     # keep only abi3
     if str(whl).find('-wasm32')>0:
         if str(whl).find('-abi3-')<0:
             continue
+        else:
+            abi3=True
 
-    for replace in ("-cp310", "-cp311", "-cp312", "-cp313"):
-        whlname = whlname.replace(replace, "-<abi>")
+    if not abi3:
+        for replace in ("-cp310", "-cp311", "-cp312", "-cp313"):
+            whlname = whlname.replace(replace, "-<abi>")
 
     process_wheel(whl, whlname)
 
